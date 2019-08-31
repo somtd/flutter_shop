daf856f6d5f773c57f86614117da9c6429b0c904
somtd <somtd1104@me.com>
2019-08-31 23:54:27 +0900

    Adding expand function with stateful widget

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,15 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as orders;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   //WidgetのOrderItemとProviderのOrderクラスのOrderItemを識別するため
   final orders.OrderItem order;
 
   OrderItem(this.order);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -17,14 +24,22 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('yyyy MM dd hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('yyyy MM dd hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 100, 180),
+          
         ],
       ),
     );
